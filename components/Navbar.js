5c62709222401190f1d01318163ca34cc47e0980
iwgx <wahabiputra@gmail.com>
2019-04-14 23:45:34 +0700

    Change form structure

--- components/Navbar.js
+++ components/Navbar.js
@@ -24,16 +24,20 @@ class Navbar extends Component {
 					className="form-inline collapse navbar-collapse"
 				>
 					<div className="input-group ml-auto">
+						<input type="text" class="form-control" placeholder="Keyword" />
+					</div>
+					<div className="input-group mx-2">
 						<select class="custom-select" id="country-select">
 							<option selected>Choose location</option>
 							<option value="id">Indonesia</option>
 							<option value="us">United States</option>
 						</select>
 					</div>
-					<div className="btn-group mx-3">
-						<button type="button" className="btn btn-primary">React</button>
-						<button type="button" className="btn btn-danger">Angular</button>
-						<button type="button" className="btn btn-success">Vue</button>
+					<div class="form-check mx-2">
+						<input class="form-check-input" type="checkbox" id="inlineFormCheck" />
+						<label class="form-check-label" for="inlineFormCheck">
+							Full time
+						</label>
 					</div>
 					<button type="submit" className="btn btn-outline-dark">Search</button>
 				</form>
