c30c08ca28b51afa328d3cadd938623729a258b0
iwgx <wahabiputra@gmail.com>
2019-04-16 08:29:24 +0700

    Fix card height and responsiveness issue

--- components/Job.js
+++ components/Job.js
@@ -1,8 +1,8 @@
 import { h } from 'preact';
 
 const Jobs = ({ id, title, company, location }) => (
-	<div className="col-6 mb-2">
-		<div key={id} className="card">
+	<div className="col-12 col-sm-6 mb-2">
+		<div key={id} className="card h-100">
 			<div className="card-body">
 				<h5 className="card-title">{title}</h5>
 				<h6 className="card-subtitle text-muted">
