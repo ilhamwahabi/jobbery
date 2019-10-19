37614fbd9f6488972e12e9b7fdf86fdf6be616ad
Ilham Wahabi <wahabiputra@gmail.com>
2019-10-19 19:37:00 +0700

    enable redux dev tools in dev

--- store/index.js
+++ store/index.js
@@ -1,8 +1,14 @@
 import createStore from 'unistore';
+import devtools from 'unistore/devtools';
 
-const store = createStore({
+const initialState = {
 	jobs: [],
 	form: { keyword: '', location: 'us', isFullTime: false }
-});
+};
+
+const store =
+	process.env.NODE_ENV === 'production'
+		? createStore(initialState)
+		: devtools(createStore(initialState));
 
 export default store;
