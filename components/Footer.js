9b5b0097c88e2536cc3e14027c106582feede65a
iwgx <wahabiputra@gmail.com>
2020-02-26 06:04:25 +0700

    Make footer year dynamic

--- components/Footer.js
+++ components/Footer.js
@@ -1,18 +1,26 @@
 import { h } from 'preact';
 
-const Footer = () => (
-	<div className="container-fluid bg-light py-3 text-center">
-		Made by Ilham Wahabi 2018
-		<a
-			href="https://github.com/iwgx/jobberry"
-			target="_blank" rel="noopener noreferrer"
-		>
-			<img
-				className="ml-2 mb-2" src="../assets/github.png"
-				alt="github" width="20" height="20"
-			/>
-		</a>
-	</div>
-);
+const Footer = () => {
+	const currentYear = new Date().getFullYear();
+
+	return (
+		<div className="container-fluid bg-light py-3 text-center">
+			Made by Ilham Wahabi {currentYear}
+			<a
+				href="https://github.com/iwgx/jobberry"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<img
+					className="ml-2 mb-2"
+					src="../assets/github.png"
+					alt="github"
+					width="20"
+					height="20"
+				/>
+			</a>
+		</div>
+	);
+};
 
 export default Footer;
